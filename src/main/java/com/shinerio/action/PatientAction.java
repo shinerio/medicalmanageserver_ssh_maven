c56b2944c62385b5d9663dbc6e95181b1115078a
shinerio <jstxzhangrui@163.com>
2016-12-21 22:36:00 +0800

    2016-12-21 22:35:58

--- src/main/java/com/shinerio/action/PatientAction.java
+++ src/main/java/com/shinerio/action/PatientAction.java
@@ -1,6 +1,7 @@
 package com.shinerio.action;
 
 import com.opensymphony.xwork2.ActionSupport;
+import com.shinerio.domain.Evaluation_info;
 import com.shinerio.domain.Patients;
 import com.shinerio.service.PatientService;
 import net.sf.json.JSONArray;
@@ -11,6 +12,7 @@ import org.apache.struts2.interceptor.ServletResponseAware;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.support.ReplaceOverride;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,6 +20,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.WebConnection;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -94,10 +97,24 @@ public class PatientAction extends ActionSupport implements ServletRequestAware,
     }
 
     public void listHistoryData(){
-        List list = patientService.getEvaluation_infoById(1);
+        String str_start_time =  request.getParameter("start_time");
+        String str_end_time = request.getParameter("end_time");
+        int patient_id = Integer.parseInt(request.getParameter("patient_id"));
+        List<Evaluation_info>  list = patientService.getEvaluation_infoById(patient_id);
+        List<Evaluation_info> result = new ArrayList();
+        if(!("".equals(str_start_time)||str_end_time==null)) {
+            int start_time = Integer.parseInt(str_start_time);
+            int end_time = Integer.parseInt(str_end_time);
+            for (Evaluation_info e : list) {
+                if (e.getStart_time() >= start_time && e.getStart_time() <= end_time)
+                    result.add(e);
+            }
+        }else{
+            result = list;
+        }
         JsonConfig jsonConfig = new JsonConfig();
         jsonConfig.setExcludes(new String[]{"patient"});
-        JSONArray jsonArray = JSONArray.fromObject(list,jsonConfig);
+        JSONArray jsonArray = JSONArray.fromObject(result,jsonConfig);
         try {
             PrintWriter writer = response.getWriter();
             writer.write(jsonArray.toString());
